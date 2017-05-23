2c50717a7e6097dccdbf697439523089e1f034d0
kkmike999@163.com <kkmike999@163.com>
2017-05-23 18:35:02 +0800

    创建工程

---
+++ library/src/main/java/net/kkmike/library/MyLibrary.java
@@ -0,0 +1,10 @@
+package net.kkmike.library;
+
+/**
+ * Created by kkmike999 on 2017/05/23.
+ */
+public class MyLibrary {
+    public void display() {
+        System.out.println("display");
+    }
+}
